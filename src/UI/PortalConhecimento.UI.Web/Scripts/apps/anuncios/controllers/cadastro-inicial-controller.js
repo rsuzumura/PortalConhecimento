b3ffb0088003399284c5aeece8cc0068a0098805
rsuzumura <rsuzumura@yahoo.com.br>
2015-09-27 22:59:52 -0300

    - Ajustes na primeira página de cadastro, para armazenar as informações em cookie.

--- src/UI/PortalConhecimento.UI.Web/Scripts/apps/anuncios/controllers/cadastro-inicial-controller.js
+++ src/UI/PortalConhecimento.UI.Web/Scripts/apps/anuncios/controllers/cadastro-inicial-controller.js
@@ -1,2 +1,10 @@
-﻿app.controller('cadastroInicialCtrl', ['$scope', function ($scope) {
+﻿app.controller('cadastroInicialCtrl', ['$scope', '$location', '$cookies', function ($scope, $location, $cookies) {
+    if ($cookies.anuncio) {
+        $scope.anuncio = $cookies.anuncio;
+    }
+
+    $scope.avancar = function () {
+        $cookies.anuncio = $scope.anuncio;
+        $location.url('proximo');
+    };
 }]);
