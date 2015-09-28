4fe1f269ea5669d9317ec647e423b09ab9c9c2be
rsuzumura <rsuzumura@yahoo.com.br>
2015-09-28 00:10:21 -0300

    - Persistência de dados no cadastro de palestra corporativa.

--- src/UI/PortalConhecimento.UI.Web/Scripts/apps/anuncios/controllers/cadastro-final-controller.js
+++ src/UI/PortalConhecimento.UI.Web/Scripts/apps/anuncios/controllers/cadastro-final-controller.js
@@ -1,19 +1,27 @@
-﻿app.controller('cadastroFinalCtrl', ['$scope', 'estadoService', 'cidadeService', 'bairroService', function ($scope, estadoService, cidadeService, bairroService) {
+﻿app.controller('cadastroFinalCtrl', ['$scope', '$location', '$cookies', 'estadoService', 'cidadeService', 'bairroService', function ($scope, $location, $cookies, estadoService, cidadeService, bairroService) {
     $scope.anuncio = {};
     $scope.estados = [];
     $scope.cidades = [];
     $scope.bairros = [];
+
+    if ($cookies.anuncio) {
+        $scope.anuncio = $cookies.anuncio;
+    }
+
     $scope.carregarCidades = function () {
         if ($scope.anuncio.EstadoId) {
             cidadeService.listarPorEstado($scope.anuncio.EstadoId)
                 .success(function (data) {
                     $scope.cidades = data;
+                    if ($scope.anuncio.CidadeId) {
+                        $scope.carregarBairros();
+                    }
                 });
         } else {
             $scope.cidades = [];
+            $scope.anuncio.CidadeId = null;
         }
-        $scope.anuncio.CidadeId = null;
-    }
+    };
 
     $scope.carregarBairros = function () {
         if ($scope.anuncio.CidadeId) {
@@ -23,12 +31,20 @@
                 });
         } else {
             $scope.bairros = [];
+            $scope.anuncio.BairroId = null;
         }
-        $scope.anuncio.BairroId = null;
-    }
+    };
+
+    $scope.voltar = function () {
+        $cookies.anuncio = $scope.anuncio;
+        $location.url('proximo');
+    };
 
     estadoService.listar()
         .success(function (data) {
             $scope.estados = data;
+            if ($scope.anuncio.EstadoId) {
+                $scope.carregarCidades();
+            }
         });
 }]);
--- src/UI/PortalConhecimento.UI.Web/Scripts/apps/anuncios/controllers/cadastro-proximo-controller.js
+++ src/UI/PortalConhecimento.UI.Web/Scripts/apps/anuncios/controllers/cadastro-proximo-controller.js
@@ -1,16 +1,37 @@
 ﻿app.controller('cadastroProximoCtrl', ['$scope', '$cookies', '$location', function ($scope, $cookies, $location) {
-    $scope.anuncio = {
-        DiaUtilInicio: 0,
-        DiaUtilFim: 0,
-        FimDeSemanaFeriadoInicio: 0,
-        FimDeSemanaFeriadoFim: 0
-    };
+    $scope.anuncio = {};
+    if ($cookies.anuncio) {
+        $scope.anuncio = $cookies.anuncio;
+    }
+
+    if (!$scope.anuncio.DiaUtilInicio) {
+        $scope.anuncio.DiaUtilInicio = 0;
+    }
+    if (!$scope.anuncio.DiaUtilFim) {
+        $scope.anuncio.DiaUtilFim = 0;
+    }
+    if (!$scope.anuncio.FimDeSemanaFeriadoInicio) {
+        $scope.anuncio.FimDeSemanaFeriadoInicio = 0;
+    }
+    if (!$scope.anuncio.FimDeSemanaFeriadoFim) {
+        $scope.anuncio.FimDeSemanaFeriadoFim = 0;
+    }
 
     $scope.formatarHora = function (valor) {
         var hora = "0" + parseInt(valor / 60).toString();
         var minutos = "0" + (valor % 60).toString();
         return right(hora, 2) + ":" + right(minutos, 2);
-    }
+    };
+
+    $scope.voltar = function () {
+        $cookies.anuncio = $scope.anuncio;
+        $location.url('/');
+    };
+
+    $scope.avancar = function () {
+        $cookies.anuncio = $scope.anuncio;
+        $location.url('final');
+    };
 
     function right(value, size) {
         if (size <= 0) {
