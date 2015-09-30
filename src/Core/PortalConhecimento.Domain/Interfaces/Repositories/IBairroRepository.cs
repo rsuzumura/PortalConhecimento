8f9c65b798c6c6ca0b87e0b039b9a178dc2d363a
rsuzumura <rsuzumura@yahoo.com.br>
2015-09-29 22:03:21 -0300

    - Inclusão de interfaces de consulta de bairro e estado.

---
+++ src/Core/PortalConhecimento.Domain/Interfaces/Repositories/IBairroRepository.cs
@@ -0,0 +1,10 @@
+﻿using PortalConhecimento.Domain.Entities;
+using System.Linq;
+
+namespace PortalConhecimento.Domain.Interfaces.Repositories
+{
+    public interface IBairroRepository : IBaseRepository<Bairro, int>
+    {
+        IQueryable<Bairro> ListarPorCidade(int cidadeId);
+    }
+}
---
+++ src/Core/PortalConhecimento.Domain/Interfaces/Repositories/IEstadoRepository.cs
@@ -0,0 +1,8 @@
+﻿using PortalConhecimento.Domain.Entities;
+
+namespace PortalConhecimento.Domain.Interfaces.Repositories
+{
+    public interface IEstadoRepository : IBaseRepository<Estado, int>
+    {
+    }
+}
